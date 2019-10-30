81331385be99c4a95c49a030a202df662a2ad1f1
erickoficial69@gmail.com <erickoficial69@gmail.com>
2019-10-30 03:11:14 -0430

    borrado build react

--- src/build/service-worker.js
+++
@@ -1,9 +0,0 @@
-importScripts("/precache-manifest.25832e2e10ce89b4f810a891e83f3e19.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
-
-self.__precacheManifest = [].concat(self.__precacheManifest || [])
-workbox.precaching.precacheAndRoute(self.__precacheManifest, {})
-
-workbox.routing.registerNavigationRoute('/index.html')
-
-workbox.routing.registerRoute(/^https?.*/,
-    new workbox.strategies.NetworkFirst(),'GET')
