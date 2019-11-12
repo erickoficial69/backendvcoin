9c86895a0a375752d4eb03741df3c60c9b341693
Erick Diaz <erickoficial69@gmail.com>
2019-11-12 06:58:28 -0400

    delete build

--- src/build/service-worker.js
+++
@@ -1,9 +0,0 @@
-importScripts("./precache-manifest.c6f93f372fb9e61b053b2cb25881f263.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
-
-self.__precacheManifest = [].concat(self.__precacheManifest || [])
-workbox.precaching.precacheAndRoute(self.__precacheManifest, {})
-
-workbox.routing.registerNavigationRoute('/index.html')
-
-workbox.routing.registerRoute(/^https?.*/,
-    new workbox.strategies.NetworkFirst(),'GET')
