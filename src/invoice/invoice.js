e3bae1f27eecce2c1f62bfc2cff3bb166e7cf145
erickoficial69 <erickoficial69@gmail.com>
2019-11-16 21:24:18 +0000

    modificado invoice

--- src/invoice/invoice.js
+++ src/invoice/invoice.js
@@ -1,7 +1,9 @@
+
 module.exports = (pedido) => {
     
 return `
-    <html>
+<!DOCTYPE html>
+<html>
 
 <head>
     <meta charset="utf-8">
@@ -11,8 +13,8 @@ return `
     <meta name="author" content="">
     <style>
 
-        article{
-            font-family: roboto;
+        body{
+            font-family: sans-serif;
         }
 
         h1,
@@ -45,56 +47,60 @@ return `
             text-transform: uppercase;
             word-wrap: break-word !important;
         }
-        .flecha{
+        /.flecha{
             width: 30px;
             height: 30px;
-         
             background:linear-gradient(315deg, white 50%, #01578c 50%);
+            background-repeat: no-repeat;
+            background-size: cover;
             position: relative;
             transform: rotate(135deg);
             overflow: hidden;
             border: 1px solid white;
-            margin: 0 0 0 -10px;
-        }
-        .DetallesTabla {
-            position: relative;
+            margin: 100px 0 0 -10px;
         }
 
-        .DetallesTabla>div {
-            width: 90%;
-            position: relative;
-            margin: 0 auto;
-            display: flex;
-            justify-content: center;
-            align-items: center;
-        }
-
-        .DetallesTabla>div>img {
-            padding-right: 15px;
+        /*-------------------------------------------
+-------------   Detalles table ------------------
+---------------------------------------------*/
+        .Cabecera{
+            background-color: rgb(16, 108, 160);
+            margin: 0;
+            padding: 0;
             height: 60px;
         }
-
-        .DetallesTabla>div>span {
-            display: flex;
-            flex-flow: column;
-            align-items: flex-start;
+        .Cabecera>img{
+            height: 50px;
+            padding: 5px 10px;
+            float:left;
         }
-
-        .DatosDetalles {
-            display: grid;
-            grid-template-columns: 1fr 25px 1fr;
-            background-color: white;
-            justify-content: center;
-            align-items: center;
+        .Cabecera>h1{
+            color: white;
         }
-
+        .DatosDetalles{
+            
+            position: relative;
+            margin: 0 auto;
+            width: 90%;
+        }
         .DatosDetalles>div {
             background-color: #e1f5fc;
-            margin: 10px 20px;
+            margin: 10px 5px;
             padding: 20px;
-            height: 90%;
+            width: 295px;
+            height: 210px;
             border-radius: 10px;
-
+        }
+        .Remitente{
+          float: left;
+        }
+        .F{
+            max-width: 25px;
+            display: none;
+        }
+        .Destino{
+           float: right;
+            
         }
 
         .DatosDetalles>div>h2 {
@@ -123,18 +129,24 @@ return `
             width: 90%;
             margin: 0 auto;
         }
-
-        .DatosDetalles>span>p {
+        .Totales{
+            width: 90%;
+            margin: 0 auto;
+        }
+        .Totales>span{
+            width: 48%;
+            margin: 0 auto;
+        }
+        .Totales>span>p {
             text-align: left;
             border-bottom: 1px solid #5f8e44;
             line-height: 2;
             font-size: 13px;
             text-transform: capitalize;
-            ;
             position: relative;
         }
 
-        .DatosDetalles>span>p>span {
+        .Totales>span>p>span {
             font-weight: bold;
             position: absolute;
             right: 3%;
@@ -145,22 +157,21 @@ return `
             font-size: 16px;
             line-height: 1.8;
         }
-
-        .Botones {
-            display: flex ;
-            flex-flow: row ;
-            justify-content: flex-end !important;
-            text-align: right;
+        .TotalRemitente{
+            float: left;
+        }
+        .TotalDestino{
+             float: right;   
+        }
+        .Cortar{
+            width: 100%;
+            height: 0px;
+            padding: 30px 0 15px;
+            clear: both;
+            border-bottom: 3px dashed grey;
         }
 
-        .Botones>p {
-            font-size: 14px;
-            width: 90px;
-            text-align: center;
-            color: white;
-            margin: 1% 10px 0 0;
-            cursor: pointer;
-        }
+/*
 
         @media only screen and (max-width:720px) {
              .DatosDetalles {
@@ -172,19 +183,26 @@ return `
             transform: rotate(225deg);
             margin: 0px auto 10px;
             }
-
-            .DatosDetalles>span>p {
+            .Totales{
+                display: flex;
+                flex-flow: column;
+            }
+            .Totales>span{
+                width: 75%;
+            }
+            .Totales>span>p {
                 line-height: 3;
             }
 
-            .DatosDetalles>span>p>span {
+            .Totales>span>p>span {
                 line-height: 3;
             }
 
-            .DatosDetalles>span>p:nth-child(4) {
+            .Totales>span>p:nth-child(4) {
                 display: none;
             }
         }
+*/
     </style>
 
     <title>tablero</title>
@@ -194,44 +212,49 @@ return `
 <body>
 
     <main>
-        <article class="DatosDetalles">
-            <div>
+        <article>
+          <div class="Cabecera">
+              <img src="./img/logoTransparente.png" alt="">
+              <h1>Factura de orden #${pedido.idPedido}</h1>
+          </div>
+           <div class="DatosDetalles">
+            <div class="Remitente">
                 <h2>Remitente</h2>
                 <p>
-                    <span>Pais:</span> Argentina
+                    <span>Moneda:</span> ${pedido.monedaDeposito}
                     <br>
-                    <span>Nombres:</span> ernesto medina
+                    <span>Nombres:</span> ${pedido.nombreUsuario}
                     <br>
-                    <span>C.I:</span> 85.265.545
+                    <span>Doc. Identidad:</span> ${pedido.dniUsuario}
                     <br>
-                    <span>Direccion:</span> Calle san francisco de estaban,castellon ,españa.
+                    <span>Telefono:</span> ${pedido.telefonoUsuario}
                     <br>
-
-                    <span>telefono:</span> 0421 215 22154
+                    <span>Referencia Deposito:</span> ${pedido.referenciaDeposito?pedido.referenciaDeposito:'por definir'}
                 </p>
             </div>
-            <span><div class="flecha"></div></span>
-            <div>
-                <h2>destinatario</h2>
+            <span class="F"><div class="flecha"></div></span>
+            <div class="Destino">
+                <h2>${pedido.status==='pagada' || pedido.status==='completada' ?'destinatario':'Cuenta Vcoin'}</h2>
                 <p>
-                    <span>Pais:</span> Venezuela
+                    <span>Pais:</span> ${pedido.paisVcoin?pedido.paisVcoin:''}
                     <br>
-                    <span>Nombres:</span> ernesto medina
+                    <span>Nombres:</span> ${pedido.titularVcoin?pedido.titularVcoin:''}
                     <br>
-                    <span>C.I:</span> 85.265.545
+                    <span>Doc. IDENTIDAD</span> ${pedido.dniTitularVcoin?pedido.dniTitularVcoin:''}
                     <br>
-                    <span>Banco:</span> BBVAprovincial
+                    <span>Banco:</span> ${pedido.bancoVcoin?pedido.bancoVcoin:''}
                     <br>
                     <span>Numero de cuenta:</span>
-                    0108621414123151
+                    ${pedido.nCuentaVcoin?pedido.nCuentaVcoin:''}
                     <br>
-
                 </p>
             </div>
-            <span>
+            </div>
+            <div class="Totales">
+            <span class="TotalRemitente">
                 <p>
                     monto: <span>
-                        ${pedido.montoDeposito} ${pedido.monedaDeposito}
+                        ${pedido.montoDeposito}
                     </span>
                 </p>
                 <p>
@@ -245,11 +268,15 @@ return `
                     </span>
                 </p>
             </span>
-            <br />
-            <span>
+            <span class="TotalDestino">
+            <p>
+                    Fecha: <span>
+                        ${pedido.fechaPedido}
+                    </span>
+                </p>
                 <p>
                     referencia bancaria: <span>
-                        Definir por administrador
+                        ${pedido.referenciaRetiro?pedido.referenciaRetiro:'por definir'}
                     </span>
                 </p>
                 <p>
@@ -258,11 +285,90 @@ return `
                     </span>
                 </p>
             </span>
+            </div>
+        </article>
+        
+        <div class="Cortar"></div>
+
+        <article>
+          <div class="Cabecera">
+              <img src="./img/logoTransparente.png" alt="">
+              <h1>Factura de orden #${pedido.idPedido}</h1>
+          </div>
+           <div class="DatosDetalles">
+            <div class="Remitente">
+            <h2>Remitente</h2>
+            <p>
+                <span>Moneda:</span> ${pedido.monedaDeposito}
+                <br>
+                <span>Nombres:</span> ${pedido.nombreUsuario}
+                <br>
+                <span>Doc. Identidad:</span> ${pedido.dniUsuario}
+                <br>
+                <span>Telefono:</span> ${pedido.telefonoUsuario}
+                <br>
+                <span>Referencia Deposito:</span> ${pedido.referenciaDeposito?pedido.referenciaDeposito:'por definir'}
+            </p>
+            </div>
+            <span class="F"><div class="flecha"></div></span>
+            <div class="Destino">
+                <h2>${pedido.status==='pagada' || pedido.status==='completada' ?'destinatario':'Cuenta Vcoin'}</h2>
+                <p>
+                    <span>Pais:</span> ${pedido.paisVcoin?pedido.paisVcoin:''}
+                    <br>
+                    <span>Nombres:</span> ${pedido.titularVcoin?pedido.titularVcoin:''}
+                    <br>
+                    <span>Doc. IDENTIDAD</span> ${pedido.dniTitularVcoin?pedido.dniTitularVcoin:''}
+                    <br>
+                    <span>Banco:</span> ${pedido.bancoVcoin?pedido.bancoVcoin:''}
+                    <br>
+                    <span>Numero de cuenta:</span>
+                    ${pedido.nCuentaVcoin?pedido.nCuentaVcoin:''}
+                    <br>
+                </p>
+            </div>
+            </div>
+            <div class="Totales">
+            <span class="TotalRemitente">
+                <p>
+                    monto: <span>
+                        ${pedido.montoDeposito}
+                    </span>
+                </p>
+                <p>
+                    Taza del dia: <span>
+                        ${pedido.tazaCambio}
+                    </span>
+                </p>
+                <p>
+                    Operador: <span>
+                        ${pedido.nombreOperador}
+                    </span>
+                </p>
+            </span>
+            <span class="TotalDestino">
+            <p>
+                    Fecha: <span>
+                        ${pedido.fechaPedido}
+                    </span>
+                </p>
+                <p>
+                    referencia bancaria: <span>
+                    ${pedido.referenciaRetiro?pedido.referenciaRetiro:'por definir'}
+                    </span>
+                </p>
+                <p>
+                    total recibir: <span className="TotalDestino">
+                        ${pedido.montoRetiro} bsf
+                    </span>
+                </p>
+            </span>
+            </div>
         </article>
         
     </main>
 </body>
 
 </html>
-    `;
+`;
 };
